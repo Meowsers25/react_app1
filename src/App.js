f01da94a3e779b538618a78fe13976e2c5e66c41
Chris <c.lamothe25@gmail.com>
2017-12-17 11:27:56 -0500

    understanding jsx

--- src/App.js
+++ src/App.js
@@ -8,6 +8,11 @@ class App extends Component {
         <h1>Hi, I'm a React App</h1>
       </div>
     );
+
+    //This is the same as the abve code.
+    // The above code is jsx which gets compiled
+    //to the code written below
+    //return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
   }
 }
 
